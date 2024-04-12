30add0c1498286c7613a92337207919ec12330c5
YangSingLimbu <yangsinglimbu@gmail.com>
2024-04-12 09:58:27 +0545

    updated app.js

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
-const createDOMPurify = require('dompurify');
-const { JSDOM } = require('jsdom');
-
+const createDOMPurify = require("dompurify");
+const { JSDOM } = require("jsdom");
+const path = require("path");
 
 let posts = [];
 
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,10 +18,9 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("home", {
-    postsContent: posts
+    postsContent: posts,
   });
 });
-
 app.get("/about", (req, res) => {
   res.render("about", {});
 });
@@ -30,18 +30,18 @@ app.get("/contact", (req, res) => {
 });
 
 app.get("/compose", (req, res) => {
-    res.render("compose");
+  res.render("compose");
 });
 
 app.post("/compose", (req, res) => {
   const Compose = {
     title: req.body.title,
-    content: req.body.body
-  }
-  const window = new JSDOM('').window;
+    content: req.body.body,
+  };
+  const window = new JSDOM("").window;
   const DOMPurify = createDOMPurify(window);
   Compose.content = DOMPurify.sanitize(Compose.content);
-  
+
   posts.push(Compose);
   res.redirect("/");
 });
@@ -49,21 +49,16 @@ app.post("/compose", (req, res) => {
 app.get("/posts/:title", (req, res) => {
   var urlTitle = _.lowerCase(req.params.title);
 
-  posts.forEach( x => {
-    if( _.lowerCase(x.title) === urlTitle) {
+  posts.forEach((x) => {
+    if (_.lowerCase(x.title) === urlTitle) {
       res.render("post.ejs", {
         postTitle: x.title,
-        postBody: x.content
-      })
-    } 
-  })
-
-
+        postBody: x.content,
+      });
+    }
+  });
 });
-
-
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
-
